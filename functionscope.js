6fbafcb36060d991a78e0cd5d3467175615cb07f
Ishan Mahajan <im530@cam.ac.uk>
2024-11-18 20:32:18 +0000

    functionscope js add

---
+++ functionscope.js
@@ -0,0 +1,26 @@
+var data = "Hello World"
+
+function display(){
+    var data = "Hello from function";
+    console.log(data)
+}
+
+// display()
+
+function notes(){
+     display();
+     var data = "Hello from notes function";
+     display();
+}
+
+notes();
+
+function tweetData(a, b){
+    function addition(a, b){
+        return a + b
+    }
+    return addition(a, b)
+}
+
+console.log(tweetData(5, 9));
+console.log(addition(5, 9));
