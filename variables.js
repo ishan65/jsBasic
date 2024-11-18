e5869ada72ae2f784cb545086eb87a559b64c465
Ishan Mahajan <im530@cam.ac.uk>
2024-11-18 13:23:37 +0000

    variable js updated

--- variables.js
+++ variables.js
@@ -1,2 +1,12 @@
-var country = "India";
-console.log("My place of birth is "+country);
+var namePerson;
+namePerson = "Ishan";
+
+const country = "India";
+
+console.log("my name is "+ namePerson +" and my birthplace is "+ country);
+
+namePerson = "Neelam";
+console.log("my name is "+ namePerson +" and my birthplace is "+ country);
+
+let webDevelopment = "In process";
+console.log(webDevelopment);
