b88f6ab9b27c772f80eb0396b66f8c683dd9ae32
Ishan Mahajan <im530@cam.ac.uk>
2024-11-18 14:32:45 +0000

    string js add

---
+++ string.js
@@ -0,0 +1,27 @@
+var firstName = "Ishan"
+var lastName = "Mahajan"
+
+var fullname;
+fullname = firstName +" "+lastName;
+console.log(fullname)
+
+var mickey = 'Say\'s hi';
+console.log(mickey);
+
+var mickey = 'Say\\s hi';
+console.log(mickey);
+
+/*
+code output
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
+
+var test = firstName + "\b" + lastName;
+console.log(test);
