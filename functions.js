d7b048a101e8392ea9af486909562d266f2fe4e2
Ishan Mahajan <im530@cam.ac.uk>
2024-11-18 20:03:58 +0000

    functions js add

---
+++ functions.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    return a + b
+}
+
+console.log(add(3,4));
+console.log(add(13,14));
+
+subtraction = (a, b) => {
+    return a - b
+}
+console.log(subtraction(13,4));
+console.log(subtraction(20,14));
+
+divison = (a, b) => a/b
+console.log(divison(20,5));
+console.log(divison(20,3));
