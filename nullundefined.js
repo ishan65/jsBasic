e5fec17110829f8fe3446a883e835c3a361e9e13
Ishan Mahajan <im530@cam.ac.uk>
2024-11-19 12:43:27 +0000

    nullundefined js add

---
+++ nullundefined.js
@@ -0,0 +1,10 @@
+// null and undefined
+var good;
+console.log(good);
+var bad = null;
+console.log(bad);
+console.log(good === bad)
+console.log(typeof(good))
+console.log(typeof(bad))
+console.log(5+bad);
+console.log(5+good);
