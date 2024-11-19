7edb6ea5fc30fa499c8b5362a4b13224d803754f
Ishan Mahajan <im530@cam.ac.uk>
2024-11-19 16:29:31 +0000

    arrayfunctions js add

---
+++ arrayfunctions.js
@@ -0,0 +1,33 @@
+var number = [100, 200, 300, 400];
+
+// push
+number.push(500);
+console.log(number);
+// pop
+console.log(number.pop());
+console.log(number);
+// concat
+number2 = [1000, 2000, 3000];
+console.log(number.concat(number2));
+console.log(number);
+// join
+console.log(number.join());
+console.log(number.join("|"));
+// reverse
+console.log(number2.reverse());
+console.log(number2)
+//sort
+console.log(number2.sort());
+console.log(number2)
+//shift
+console.log(number2.shift())
+console.log(number2)
+//unshift
+console.log(number2.unshift(1001))
+console.log(number2)
+// slice
+console.log(number.slice(0,2))
+console.log(number)
+// splice
+console.log(number.splice(0,2, 150))
+console.log(number)
