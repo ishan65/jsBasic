729016e45bfe6e41e4e9248fd9db3d97dde2a325
Ishan Mahajan <im530@cam.ac.uk>
2024-11-19 15:27:08 +0000

    switch js add

---
+++ switch.js
@@ -0,0 +1,16 @@
+var task = "Vegies";
+switch (task){
+    case "grosary":
+        console.log("alot of shopping")
+    case "Vegies":
+        console.log("Going to supermarket")
+        break;
+    case "car":
+        console.log("Going to Garage")
+        break;
+    case "shop":
+        console.log("I am going to market")
+        break;
+    default:
+        console.log("stay at home")
+}
